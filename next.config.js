7215e49b4e0afc0ece1ef3409c722fe970f592a9
Jonathan Ginter <jonathan.ginter+omcitcld@patientsandpurpose.com>
2026-02-28 19:18:13 -0500

    fix webpack config

--- next.config.js
+++ next.config.js
@@ -13,12 +13,14 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-
-  // webpack config for Docker hot reloading
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000, // check for changes every second
-      aggregateTimeout: 300, // delay before rebuild
+  
+  // Webpack configuration for Docker hot reloading
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second
+        aggregateTimeout: 300, // Delay before rebuilding
+      }
     }
     return config
   },
@@ -65,3 +67,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
